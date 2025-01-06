3699931ebb04422c1d5e29c6c34dec34ab931069
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-06 15:27:12 +0400

    Update build.sh file

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Postgres позволяет подключиться к удаленной базе указав ссылку на нее после флага -d
-# ссылка подгрузится из переменной окружения, которую нам нужно будет указать на сервисе деплоя
-# дальше мы загружаем в подключенную базу наш sql-файл с таблицами
-make install && psql -a -d $DATABASE_URL -f database.sql
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+make install
