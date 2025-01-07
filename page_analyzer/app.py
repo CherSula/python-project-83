a7c564ed1fe22aeef74ca49f19da9249c93d911a
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-07 16:45:41 +0400

    Add template to index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
-
+from dotenv import load_dotenv
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to 3rd project!'
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
