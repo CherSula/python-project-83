fa69fbbfa4e1f767e93f3c621cad57561f92308d
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-14 23:08:17 +0400

    Test config.py 2

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -8,7 +8,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = 'b51f49b0-01b8-4183-bc1e-519f98247b7f'
     # os.getenv("SECRET_KEY")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = 'postgresql://svetlana:01ESbrJ0oqPG8VL9yH3l5UkU9SL2MHcI@dpg-cu2h8l1opnds738jsu1g-a/page_analyzer_2shw'
+    # os.environ.get("DATABASE_URL")
 
     def __init__(self):
         if not self.SECRET_KEY:
