00e8fe3edaafd47b30bc0fc7fda74742349295ee
Svetlana Chernysheva <chers1303@gmail.com>
2025-01-14 22:37:25 +0400

    Test build.sh with exact link

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install && psql -a -d 'postgresql://svetlana:01ESbrJ0oqPG8VL9yH3l5UkU9SL2MHcI@dpg-cu2h8l1opnds738jsu1g-a/page_analyzer_2shw' -f database.sql
